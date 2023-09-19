59280f9553aebf0d5a527d022056350d3fd3cd64
Sartvas <sartvas@yandex.ru>
2023-09-19 08:30:07 +0300

    Learn JSON

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-$mysqli = mysqli_connect('localhost', 'root', '', 'mydb');
-
-/* Set the desired charset after establishing a connection */
-mysqli_set_charset($mysqli, 'utf8mb4');
-
-printf("Success... %s\n", mysqli_get_host_info($mysqli));
-
 
 
 ?>
--- jsonabout.php
+++ jsonabout.php
@@ -1,5 +1,114 @@
-<?php 
-https://api.calltouch.ru/calls-service/RestAPI/requests/41648/register/   
+<?php
 
+$data = [1, 2, 3];
+
+$data2 = [
+    'x' => 1,
+    'y' => 2,
+    'z' => 3,
+];
+
+$data3 = '[
+    "x",
+    "y",
+    "z"
+]';
+
+$json_obj = '{
+    "a": 1,
+    "b": 2,
+    "c": 3
+}';
+
+$json = json_encode($data2);
+$my_jsone_decode = json_decode($json_obj);
+
+//$json_obj При преобразовании объектов JSON есть нюансы. Дело в том, что они преобразуются не в ассоциативные массивы PHP, а в объекты PHP.
+
+var_dump($my_jsone_decode);
+
+echo "<br>";
+
+$json_ex_1 = '{
+    "user": {
+        "name": "john",
+        "surn": "smit"
+    },
+    "city": "London"
+}';
+
+$my_jsone_decode_2 = json_decode($json_ex_1);
+
+echo $my_jsone_decode_2->user->name . " " . $my_jsone_decode_2->user->surn . " " .$my_jsone_decode_2->city;
+
+echo "<br>";
+
+$json_ex_2 ='{
+    "ru": ["пн", "вт", "ср"],
+    "en": ["mn", "tu", "wd"]
+}';
+
+$json_ex_2_decode = json_decode($json_ex_2);
+
+
+echo($json_ex_2_decode->ru[2]);
+echo "<br>";
+foreach($json_ex_2_decode->en as $item){
+    echo $item . " ";
+}
+
+echo "<br>";
+
+//Можно сделать так, чтобы объекты JSON преобразовывались в ассоциативные массивы в PHP. Для этого нужно второй параметр функции json_decode установить в значение true.
+
+$json_expl_1 = '{
+    "ru": ["пн", "вт", "ср"],
+    "en": ["mn", "tu", "wd"]
+}';
+
+
+$json_expl_1_decode = json_decode($json_expl_1, true);
+
+echo $json_expl_1_decode['ru'][1];
+
+$json_expl_2 = [1, 2, 3];
+	$json_expl_2  = json_encode($data);
+	
+	header('Content-Type: application/json');
+	echo $json_expl_2;
+
+$json_wrong = '["a", "b", "c",]';
+
+$json_wrong_decode = json_decode($json_wrong);
+
+var_dump($json_wrong_decode);
+
+$error = json_last_error(); //Так как возникала ошибка, то json_last_error при вызове выдаст номер этой ошибки:
+
+echo "<br>";
+
+switch (json_last_error()) {
+    case JSON_ERROR_NONE:
+        echo 'ошибок нет';
+    break;
+    case JSON_ERROR_DEPTH:
+        echo 'достигнута максимальная глубина стека';
+    break;
+    case JSON_ERROR_STATE_MISMATCH:
+        echo 'некорректные разряды или несоответствие режимов';
+    break;
+    case JSON_ERROR_CTRL_CHAR:
+        echo 'некорректный управляющий символ';
+    break;
+    case JSON_ERROR_SYNTAX:
+        echo 'синтаксическая ошибка, некорректный JSON';
+    break;
+    case JSON_ERROR_UTF8:
+        echo 'некорректные символы UTF-8, возможно неверно закодирован';
+    break;
+    default:
+        echo 'неизвестная ошибка';
+    break;
+}
 
 ?>
